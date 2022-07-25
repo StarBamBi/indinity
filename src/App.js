301ff3265cac38e7d64a0c46b51063b4ac0e6456
StarBamBi <59411333+StarBamBi@users.noreply.github.com>
2022-07-25 13:49:53 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,206 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import CalculateIcon from "@mui/icons-material/Calculate";
+import SummarizeIcon from "@mui/icons-material/Summarize";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [inputBtnValue, setInputBtnValue] = useState(""); // 계산기 버튼 값 입력
+  let [result, setResult] = useState(0); // 결과값
+  let [history, setHistory] = useState([]); // 계산 과정 기록
+
+  function addInput(num) {
+    // 계산 과정 함수
+    let input = inputBtnValue + num;
+    setInputBtnValue(input);
+    history.push(input);
+    setHistory(history);
+  }
+  function removeInput() {
+    // 계산 결과 지우기
+    setInputBtnValue(inputBtnValue.slice(0, -1));
+    history.push(inputBtnValue.slice(0, -1));
+    setHistory(history);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <div>
+          <Typography variant="h4" gutterBottom component="div">
+            계산기
+          </Typography>
+          <Divider sx={{ mt: 2, mb: 5 }} />
+        </div>
+        <div>
+          <div
+            className="result-section"
+            style={{ textAlign: "right", width: "285px" }}
+          >
+            <div style={{ fontSize: "16px", color: "rgb(187 186 186)" }}>
+              {inputBtnValue}
+            </div>
+            <div style={{ fontSize: "45px", color: "#f34f4f" }}>{result}</div>
+          </div>
+          <div>
+            <Stack direction="row" spacing={2} sx={{ my: 1 }}>
+              <button
+                className="cal-btn"
+                style={{ background: "#b9b8b8" }}
+                onClick={() => setInputBtnValue("")}
+              >
+                C
+              </button>
+              <button className="cal-btn" style={{ background: "#b9b8b8" }}>
+                %
+              </button>
+              <button className="cal-btn" style={{ background: "#b9b8b8" }}>
+                ÷
+              </button>
+              <button
+                className="cal-btn"
+                style={{ background: "rgb(251 172 172)" }}
+              >
+                X
+              </button>
+            </Stack>
+            <Stack direction="row" spacing={2} sx={{ my: 1 }}>
+              <button
+                className="cal-btn"
+                style={{ background: "#b9b8b8" }}
+                onClick={() => addInput("7")}
+              >
+                7
+              </button>
+              <button
+                className="cal-btn"
+                style={{ background: "#b9b8b8" }}
+                onClick={() => addInput("8")}
+              >
+                8
+              </button>
+              <button
+                className="cal-btn"
+                style={{ background: "#b9b8b8" }}
+                onClick={() => addInput("9")}
+              >
+                9
+              </button>
+              <button
+                className="cal-btn"
+                style={{ background: "rgb(251 172 172)" }}
+                onClick={() => addInput("-")}
+              >
+                ㅡ
+              </button>
+            </Stack>
+            <Stack direction="row" spacing={2} sx={{ my: 1 }}>
+              <button
+                className="cal-btn"
+                style={{ background: "#b9b8b8" }}
+                onClick={() => addInput("4")}
+              >
+                4
+              </button>
+              <button
+                className="cal-btn"
+                style={{ background: "#b9b8b8" }}
+                onClick={() => addInput("5")}
+              >
+                5
+              </button>
+              <button
+                className="cal-btn"
+                style={{ background: "#b9b8b8" }}
+                onClick={() => addInput("6")}
+              >
+                6
+              </button>
+              <button
+                className="cal-btn"
+                style={{ background: "rgb(251 172 172)" }}
+                onClick={() => addInput("+")}
+              >
+                +
+              </button>
+            </Stack>
+            <Stack direction="row" spacing={2} sx={{ my: 1 }}>
+              <button
+                className="cal-btn"
+                style={{ background: "#b9b8b8" }}
+                onClick={() => addInput("1")}
+              >
+                1
+              </button>
+              <button
+                className="cal-btn"
+                style={{ background: "#b9b8b8" }}
+                onClick={() => addInput("2")}
+              >
+                2
+              </button>
+              <button
+                className="cal-btn"
+                style={{ background: "#b9b8b8" }}
+                onClick={() => addInput("3")}
+              >
+                3
+              </button>
+              <button
+                className="cal-btn"
+                style={{ background: "red", color: "white" }}
+                onClick={() => addInput("3")}
+              >
+                =
+              </button>
+            </Stack>
+            <Stack direction="row" spacing={2} sx={{ my: 1 }}>
+              <button
+                className="cal-btn"
+                style={{ background: "#b9b8b8" }}
+                onClick={() => removeInput()}
+              >
+                &lt;
+              </button>
+              <button
+                className="cal-btn"
+                style={{ background: "#b9b8b8" }}
+                onClick={() => addInput("0")}
+              >
+                0
+              </button>
+              <button
+                className="cal-btn"
+                style={{ background: "#b9b8b8" }}
+                onClick={() => addInput(".")}
+              >
+                .
+              </button>
+            </Stack>
+          </div>
+          <div>
+            <CalculateIcon />
+            <SummarizeIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function CalBtn(props) {
+  return (
+    <div>
+      <button
+        className="cal-btn"
+        style={{ background: props.bcolor, color: props.fcolor }}
+        onClick={() => props.onClick}
+      >
+        {props.btn1}
+      </button>
     </div>
   );
 }
